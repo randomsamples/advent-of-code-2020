7f7e08441068572755f3294a8abf921489f8b2d4
two sigma plus <randomsamples@github.com>
2020-12-01 19:21:07 -0800

    tests breaking

--- advent2020/tests/test_day1.py
+++ advent2020/tests/test_day1.py
@@ -1,7 +1,7 @@
-import advent2020.day1 as d
+from advent2020.day1 import report_repair
 
 
 def test_day1():
     expenses = [1721, 979, 366, 299, 675, 1456]
-    actual = d.report_repair(expenses)
+    actual = report_repair(expenses)
     assert actual == 30
