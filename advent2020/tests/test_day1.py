4f22354c51ee718f44254ed38959c1bc50936feb
two sigma plus <randomsamples@github.com>
2020-12-01 22:31:57 -0800

    Add pyassert

--- advent2020/tests/test_day1.py
+++ advent2020/tests/test_day1.py
@@ -1,14 +1,15 @@
 from advent2020.day1 import report_repair_1
 from advent2020.day1 import report_repair_2
+from assertpy import assert_that
 
 
 def test_day1_1():
     expenses = [1721, 979, 366, 299, 675, 1456]
     actual = report_repair_1(expenses)
-    assert actual == 514579
+    assert_that(actual).is_equal_to(514579)
 
 
 def test_day1_2():
     expenses = [1721, 979, 366, 299, 675, 1456]
     actual = report_repair_2(expenses)
-    assert actual == 241861950
+    assert_that(actual).is_equal_to(241861950)
